ef7c7cdbaf430735e05d2ac46395c34f1e939614
joreynajr <joreyna@live.com>
2021-11-17 18:03:13 -0800

    First push

---
+++ workflow/notebooks/development/download_from_ftp_server.sh
@@ -0,0 +1,41 @@
+# set bash strict mode 
+set -euo pipefail
+IFS=$'\n\t'
+
+outdir="results/main/cmipb_datasets/"
+mkdir -p $outdir
+
+#########################################################################################
+# Download 2020 data ####################################################################
+#########################################################################################
+#2020LD_subject.csv
+#2020LD_specimen.csv
+#2020LD_live_cell_percentages.csv
+#2020LD_olink_prot_exp.csv
+#2020LD_rnaseq.csv
+
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_subject.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_specimen.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_live_cell_percentages.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_olink_prot_exp.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_rnaseq.csv
+
+#########################################################################################
+# Download 2021 data ####################################################################
+#########################################################################################
+#2021BD_subject.csv
+#2021BD_specimen.csv
+#2021BD_live_cell_percentages.csv
+#2021BD_olink_prot_exp.csv
+#2021BD_rnaseq.csv
+
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_subject.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_specimen.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_live_cell_percentages.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_olink_prot_exp.csv
+wget -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_rnaseq.csv
+
+#########################################################################################
+# Other #################################################################################
+#########################################################################################
+#2021_submission_format.tsv
---
+++ workflow/notebooks/development/download_from_webpage_links.sh
@@ -0,0 +1,50 @@
+# setting the output directory
+outdir="results/main/cmipb_datasets/"
+
+# links are coming from: 
+# https://www.cmi-pb.org/mycmipb/2021_prediction_challenge/
+
+#########################################################################################
+# Download 2020 data ####################################################################
+#########################################################################################
+
+# get subject data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_subject.csv
+
+# get specimen data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_specimen.csv
+
+# get antibody titer data (status: temporarily skipped)
+#wget -O $outdir https://www.cmi-pb.org/api/v1/ab_titer
+
+# get rnaseq data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_rnaseq.csv
+
+# get protein data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_olink_prot_exp.csv
+
+# get cell frequencies data (status: broken link)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_cell_frequency.csv
+
+#########################################################################################
+# Download 2021 data ####################################################################
+#########################################################################################
+
+# get subject data (status: incorrect link)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020BD_subject.csv
+
+# get specimen data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_specimen.csv
+
+# get antibody titer data (note generated for 2021)
+
+# get rnaseq data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_rnaseq.csv
+
+# get protein data (status: good)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_olink_prot_exp.csv
+
+# get cell frequencies data (status: incorrect & broken link)
+wget -O $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020BD_cell_frequency.csv
+
+
