99a92de07eafcd1c810ac96db3557dd8df5b466a
joreynajr <joreyna@live.com>
2022-04-28 14:38:42 -0700

    New datafreeze created on 2022-04-27

--- workflow/scripts/download_from_webpage_links.sh
+++ workflow/scripts/download_from_webpage_links.sh
@@ -8,42 +8,55 @@ outdir="results/main/cmi_pb_datasets/raw/"
 # Download 2020 data ####################################################################
 #########################################################################################
 
-# get subject data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_subject.csv
+## get subject data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_subject.csv
+#
+## get specimen data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_specimen.csv
+#
+## get antibody titer data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_ab_titer.csv
+#
+## get rnaseq data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_rnaseq.csv
+#
+## get protein data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_olink_prot_exp.csv
+#
+## get cell frequencies data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_live_cell_percentages.csv
+#
+##########################################################################################
+## Download 2021 data ####################################################################
+##########################################################################################
+#
+## get subject data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_subject.csv
+#
+## get specimen data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_specimen.csv
+#
+## get antibody titer data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_ab_titer.csv
+#
+## get rnaseq data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_rnaseq.csv
+#
+## get protein data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_olink_prot_exp.csv
+#
+## get cell frequencies data (status: good)
+#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_live_cell_percentages.csv
 
-# get specimen data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_specimen.csv
 
-# get antibody titer data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_ab_titer.csv
+##########################################################################################
+## Download All Data from 2022-04-27 Freeze ##############################################
+##########################################################################################
+echo "Downloading all the data from 2022-04-27"
+#wget --no-check-certificate --recursive --no-parent -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/04272022/
 
-# get rnaseq data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_rnaseq.csv
-
-# get protein data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_olink_prot_exp.csv
-
-# get cell frequencies data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_live_cell_percentages.csv
-
-#########################################################################################
-# Download 2021 data ####################################################################
-#########################################################################################
-
-# get subject data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_subject.csv
-
-# get specimen data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_specimen.csv
-
-# get antibody titer data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_ab_titer.csv
-
-# get rnaseq data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_rnaseq.csv
-
-# get protein data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_olink_prot_exp.csv
-
-# get cell frequencies data (status: good)
-wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_live_cell_percentages.csv
+echo "Hardlinking csv files to the raw data files"
+#for fn in $(ls results/main/cmi_pb_datasets/raw/www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/04272022/*.csv);
+#do
+#    ln $fn results/main/cmi_pb_datasets/raw/
+#done
