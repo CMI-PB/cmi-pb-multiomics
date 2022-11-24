7af648165ca18cd90ca3c5174b2e034461c71dce
joreynajr <joreyna@live.com>
2022-11-23 16:31:01 -0800

    Updated the download scripts

--- second_challenge/workflow/scripts/download_from_webpage_links.sh
+++ second_challenge/workflow/scripts/download_from_webpage_links.sh
@@ -2,61 +2,14 @@
 outdir="results/main/cmi_pb_datasets/raw/"
 
 # links are coming from: 
-# https://www.cmi-pb.org/mycmipb/2021_prediction_challenge/
+# https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2nd_cmipb_challenge/10202022/
 
 #########################################################################################
-# Download 2020 data ####################################################################
+# Download All 2020,21,22 data ##########################################################
 #########################################################################################
-
-## get subject data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_subject.csv
-#
-## get specimen data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_specimen.csv
-#
-## get antibody titer data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_ab_titer.csv
-#
-## get rnaseq data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_rnaseq.csv
-#
-## get protein data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_olink_prot_exp.csv
-#
-## get cell frequencies data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2020LD_live_cell_percentages.csv
-#
-##########################################################################################
-## Download 2021 data ####################################################################
-##########################################################################################
-#
-## get subject data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_subject.csv
-#
-## get specimen data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_specimen.csv
-#
-## get antibody titer data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_ab_titer.csv
-#
-## get rnaseq data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_rnaseq.csv
-#
-## get protein data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_olink_prot_exp.csv
-#
-## get cell frequencies data (status: good)
-#wget --no-check-certificate -P  $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/2021BD_live_cell_percentages.csv
-
-
-##########################################################################################
-## Download All Data from 2022-04-27 Freeze ##############################################
-##########################################################################################
-echo "Downloading all the data from 2022-04-27"
-#wget --no-check-certificate --recursive --no-parent -P $outdir https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/04272022/
-
-echo "Hardlinking csv files to the raw data files"
-#for fn in $(ls results/main/cmi_pb_datasets/raw/www.cmi-pb.org/downloads/cmipb_challenge_datasets/2021_cmipb_challenge/04272022/*.csv);
-#do
-#    ln $fn results/main/cmi_pb_datasets/raw/
-#done
+wget --no-check-certificate \
+     --no-directories \
+     --recursive \
+     --no-parent \
+     --directory-prefix $outdir \
+     https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2nd_cmipb_challenge/10202022/
