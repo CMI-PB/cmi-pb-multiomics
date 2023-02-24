b427bbfb9e37b7fb8523d7d24fc927d882533b86
joreynajr <joreyna@live.com>
2023-02-23 19:14:18 -0800

    Re-running imputation with restructed data

---
+++ git.h
@@ -0,0 +1,7 @@
+git add second_challenge/workflow/notebooks/corrections/imputation_generalized.run.sh second_challenge/workflow/notebooks/corrections/imputation_generalized.Rmd
+
+git rm second_challenge/workflow/notebooks/Corrections/imputation_generalized.Rmd second_challenge/workflow/notebooks/Corrections/imputation_generalized.run.sh
+
+#
+#	second_challenge/workflow/notebooks/Standardize Data - Triway.ipynb
+#	second_challenge/workflow/notebooks/corrections/CMIPB_batch_QC_correct_2020_2021.Rmd
--- second_challenge/workflow/notebooks/corrections/imputation_generalized.run.sh
+++ second_challenge/workflow/notebooks/corrections/imputation_generalized.run.sh
@@ -1,3 +1,12 @@
+#PBS -o results/main/cmi_pb_datasets/processed/all_versus_all/imputation/
+#PBS -e results/main/cmi_pb_datasets/processed/all_versus_all/imputation/
+#PBS -N imputation_generalized
+#PBS -l walltime=200:00:00
+#PBS -l mem=20gb
+#PBS -l nodes=1:ppn=4
+#PBS -t 0-2
+
+# This script has to be run from the parent directory of imputation_generalized.Rmd
 
 # list the years to run
 # reminder: bash arrays are zero-indexed
@@ -22,4 +31,4 @@ rmarkdown_call="rmarkdown::render('${code}', params=list(year='${year}'), output
 # run the Rmd
 echo /mnt/BioApps/R/4.2.2/bin/Rscript -e "${rmarkdown_call}"
 /mnt/BioApps/R/4.2.2/bin/Rscript -e "${rmarkdown_call}"
-#Rscript -e "${rmarkdown_call}"
+#Rscript -e "${rmarkdown_call
