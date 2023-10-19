dca7f5c6a8068ae9991124a70527f86bd89592a7
joreynajr <joreyna@live.com>
2023-10-19 10:55:05 -0700

    Starting the third challenge

--- git.h
+++
@@ -1,7 +0,0 @@
-git add second_challenge/workflow/notebooks/corrections/imputation_generalized.run.sh second_challenge/workflow/notebooks/corrections/imputation_generalized.Rmd
-
-git rm second_challenge/workflow/notebooks/Corrections/imputation_generalized.Rmd second_challenge/workflow/notebooks/Corrections/imputation_generalized.run.sh
-
-#
-#	second_challenge/workflow/notebooks/Standardize Data - Triway.ipynb
-#	second_challenge/workflow/notebooks/corrections/CMIPB_batch_QC_correct_2020_2021.Rmd
---
+++ third_challenge/workflow/scripts/download_from_webpage_links.sh
@@ -0,0 +1,17 @@
+# setting the output directory
+outdir="results/main/cmi_pb_datasets/raw/"
+
+# links are coming from: 
+# https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2nd_cmipb_challenge/10202022/
+
+#########################################################################################
+# Download All 2020,21,22 data ##########################################################
+#########################################################################################
+wget --no-check-certificate \
+     --no-directories \
+     --recursive \
+     --no-parent \
+     --directory-prefix $outdir \
+     https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2nd_cmipb_challenge/11282022/
+     
+    # https://www.cmi-pb.org/downloads/cmipb_challenge_datasets/2nd_cmipb_challenge/10202022/
